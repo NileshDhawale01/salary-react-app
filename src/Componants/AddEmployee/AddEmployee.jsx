dd6134d68e5568c1247665245c42116d258a451a
nilesh dhawale <nileshdhawale1111@gmail.com>
2024-04-17 23:14:32 +0530

    api intigration for get all employee and add employee

--- src/Componants/AddEmployee/AddEmployee.jsx
+++ src/Componants/AddEmployee/AddEmployee.jsx
@@ -1,24 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './AddEmployee.css'
 
 function AddEmployee() {
+
+    const [employee, setEmployee] = useState({
+        "employeeName": "",
+        "employeeContact": "",
+        "email": "",
+        "password": "",
+        "departmentsDto": {
+            "departmentId": 1
+        }
+    })
+
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEmployee(preState => ({
+            ...preState,
+            [name]: value
+        }));
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8080/employee', employee)
+            .then(response => {
+
+                setLoading(false)
+                console.log("Success :: ", response)
+                setEmployee({
+                    "employeeName": "",
+                    "employeeContact": "",
+                    "email": "",
+                    "password": "",
+                    "departmentsDto": {
+                        "departmentId": 1
+                    }
+                })
+            })
+            .catch(error => {
+                setLoading(false)
+                setError(error)
+            })
+    }
     return (
-        <div className='form-container'>
+        <div className='form-container' onSubmit={onSubmit}>
             <div className='form-head'>
                 Add Employee Form
             </div>
             <form className='form'>
                 <div className='form-data'>
-                    <input type='text' name='employeeName' />
+                    <input type='text' name='employeeName' value={employee.employeeName} onChange={handleChange} placeholder='Employee Name' />
                 </div>
                 <div className='form-data'>
-                    <input type='email' name='email' />
+                    <input type='email' name='email' value={employee.email} onChange={handleChange} placeholder='Email' />
                 </div>
                 <div className='form-data'>
-                    <input type='number' name='departmentId' />
+                    <input type='text' name='employeeContact' value={employee.employeeContact} onChange={handleChange} placeholder='Contact' />
                 </div>
                 <div className='form-data'>
-                    <input type='password' name='password' />
+                    <input type='number' name='departmentId' value={employee.departmentsDto.departmentId} placeholder='Department' />
+                </div>
+                <div className='form-data'>
+                    <input type='password' name='password' value={employee.password} onChange={handleChange} placeholder='Password' />
                 </div>
                 <button className='form-submit' type='submit'>Add Employee</button>
             </form>
--- src/Componants/AllEmployees/AllEmployees.jsx
+++ src/Componants/AllEmployees/AllEmployees.jsx
@@ -1,25 +1,52 @@
 import React from 'react'
 import './AllEmployees.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function AllEmployees() {
+
+
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+
+  useEffect(() => {
+    axios.get(`http://localhost:8080/employee`)
+      .then(response => {
+        setLoading(false)
+        setUsers(response.data)
+      })
+      .catch(error => {
+        setLoading(false)
+        setError(error)
+      })
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error ::{error}</div>
+  }
+
   return (
     <div className='employees'>
-      <div classNameName="employee-list-container">
+      <div className="employee-list-containe">
         <ul className="employee-list">
-          <li className="employee-item">
-            <div className="employee-details">
-              <h3 className="employee-name">John Doe</h3>
-              <p className="employee-email">john.doe@example.com</p>
-              <p className="employee-department">Department: IT</p>
-            </div>
-          </li>
-          <li className="employee-item">
-            <div className="employee-details">
-              <h3 className="employee-name">Jane Smith</h3>
-              <p className="employee-email">jane.smith@example.com</p>
-              <p className="employee-department">Department: HR</p>
-            </div>
-          </li>
+          {
+            users.data.map(u => (
+              <li className="employee-item">
+                <div className="employee-details">
+                  <h3 className="employee-name">{u.employeeName}</h3>
+                  <p className="employee-email">{u.employeeContact}</p>
+                  <p className="employee-department">{u.email}</p>
+                  <p className="employee-department">{u.departmentsDto.departmentName}</p>
+                </div>
+              </li>
+            ))
+          }
         </ul>
       </div>
     </div>
